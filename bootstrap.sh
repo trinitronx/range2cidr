dafe585b4a966dfef86f80bc5aeaad404c6ac99a
James Cuzella <james.cuzella@lyraphase.com>
2020-06-11 11:56:17 -0600

    bootstrap.sh now calls autogen.sh - FULL initial autotools bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
+BOOTSTRAP_DIR=$(cd $(dirname $0) && pwd)
 
 set -e # Drop-out from execution if error occurs
 
+. $BOOTSTRAP_DIR/autogen.sh
+
 aclocal && autoconf && automake -a --add-missing
 
-printf '\nSuccess!\nNow please run: ./autogen.sh\n'
